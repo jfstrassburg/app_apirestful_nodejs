04c3bdf41c262f07798f502b5cd9839af381cad2
jfstrassburg <strassburgjuliano@outlook.com>
2023-05-01 18:06:22 -0300

    modidificado server.js para teste

--- api/routes/apiRouter.js
+++ api/routes/apiRouter.js
@@ -1,4 +1,16 @@
 const express = require ('express')
+
+/*
+//módulo Knex para realizar a conexão com o banco de dados
+const knex = require('knex')({
+    client: 'pg',
+    debug: true,
+    connection: {
+        connectionString : process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+    }
+    });
+
 //código para o registro de novos usuários
 const bcrypt = require('bcryptjs')
 apiRouter.post (endpoint + 'seguranca/register', (req, res) => {
@@ -19,18 +31,6 @@ apiRouter.post (endpoint + 'seguranca/register', (req, res) => {
         message: 'Erro ao registrar usuario - ' + err.message })
 })
 })
-
-
-//módulo Knex para realizar a conexão com o banco de dados
-const knex = require('knex')({
-    client: 'pg',
-    debug: true,
-    connection: {
-        connectionString : process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false },
-    }
-    });
-
 const jwt = require('jsonwebtoken')
 apiRouter.post(endpoint + 'seguranca/login', (req, res) => {
     knex
@@ -64,17 +64,7 @@ apiRouter.post(endpoint + 'seguranca/login', (req, res) => {
 
 
 
-/*
-let apiRouter = express.Router()
-const endpoint = '/'
-const lista_produtos = {
-    produtos: [
-        { id: 1, descricao: "Produto 1", valor: 5.00, marca: "marca " },
-        { id: 2, descricao: "Produto 2", valor: 5.00, marca: "marca " },
-        { id: 3, descricao: "Produto 3", valor: 5.00, marca: "marca " },
-    ]
-}
-*/
+
 
 //equisições de GET /api/produtos
 apiRouter.get(endpoint + 'produtos', (req, res) => {
@@ -88,7 +78,6 @@ apiRouter.get(endpoint + 'produtos', (req, res) => {
 
 
 // CRUD
-/*
 apiRouter.get(endpoint + 'produtos/:id', (req, res) => { 
     knex.select('*').from('produto')
     .then( produtos => res.status(200).json(produtos) )
@@ -122,4 +111,27 @@ apiRouter.delete(endpoint + 'produtos/:id', (req, res) => {
     })
  })
 */
+let apiRouter = express.Router()
+const endpoint = '/'
+const lista_produtos = {
+    produtos: [
+        { id: 1, descricao: "Produto 1", valor: 5.00, marca: "marca " },
+        { id: 2, descricao: "Produto 2", valor: 5.00, marca: "marca " },
+        { id: 3, descricao: "Produto 3", valor: 5.00, marca: "marca " },
+    ]
+}
+apiRouter.get (endpoint + 'produtos', function (req, res) {
+    res.status(200).json (lista_produtos)
+})
+
+
+const db_produtos = {
+    produtos: [
+    { id: 1, descricao: "Arroz parboilizado 5Kg", valor: 25.00, marca: "Tio João" },
+    { id: 2, descricao: "Maionese 250gr", valor: 7.20, marca: "Helmans" },
+    { id: 3, descricao: "Iogurte Natural 200ml", valor: 2.50, marca: "Itambé" },
+    { id: 4, descricao: "Batata Maior Palha 300gr", valor: 15.20, marca: "Chipps" },
+    { id: 5, descricao: "Nescau 400gr", valor: 8.00, marca: "Nestlé" },
+    ]
+}
 module.exports = apiRouter;
